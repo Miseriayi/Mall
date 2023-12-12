ff1d3f5840a36571b949ce7c0bd247526701930e
Miseria <YoLaTengo_@outlook.com>
2023-12-12 17:03:12 +0800

    项目初始结构

---
+++ mall-coupon/src/test/java/com/yola/mall/coupon/MallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yola.mall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
